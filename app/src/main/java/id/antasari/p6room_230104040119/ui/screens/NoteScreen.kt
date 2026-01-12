4beea11fadbcf6d950a9ad8d2d3ae7ed062c1fa3
amaliazannahtul <230104040119@mhs.uin-antasari.ac.id>
2026-01-12 18:54:28 +0800

    Create NoteScreen.kt

---
+++ app/src/main/java/id/antasari/p6room_230104040119/ui/screens/NoteScreen.kt
@@ -0,0 +1,12 @@
+app/src/main/java/id/antasari/p6room_230104040119
+├── MainActivity.kt
+├── data
+│   ├── local
+│   │   ├── entity
+│   │   ├── dao
+│   │   └── database
+│   └── repository
+│       └── NoteRepository.kt
+└── ui
+    └── screens
+        └── NoteScreen.kt
